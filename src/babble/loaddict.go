95eb0539c229a8efc4bbfe14e119368b3275201f
Terry Cox <terry@bootstrap.je>
2019-03-18 22:08:48 +0000

    fix: static word list

--- src/babble/loaddict.go
+++ src/babble/loaddict.go
@@ -1,22 +1,22 @@
 package babble
 
 import (
-	"io/ioutil"
-	"os"
+	// "io/ioutil"
+	// "os"
 	"strings"
 )
 
 func readAvailableDictionary() (words []string) {
-	file, err := os.Open("./words")
-	if err != nil {
-		panic(err)
-	}
+	// file, err := os.Open("./words")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	//
+	// bytes, err := ioutil.ReadAll(file)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	bytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		panic(err)
-	}
-
-	words = strings.Split(string(bytes), "\n")
+	words = strings.Split("win dream evening condition feed tool total basic smell valley nor double seat arrive master track parent shore division sheet substance favor connect post spend chord fat glad original share station dad bread charge proper bar offer segment slave duck instant market degree populate chick dear enemy reply drink occur support speech nature range", " ")
 	return
 }
